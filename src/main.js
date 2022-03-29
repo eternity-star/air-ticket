b07d35cc2e77ac4f73723efb1003718cef4c42f3
fuyinshen <1090028459@qq.com>
2022-03-29 09:40:38 +0800

    fix: 恢复之前因为合并代码而丢失的文件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+import VueAxios from "vue-axios";
+import qs from 'qs';
+import moment from 'moment'
+import less from 'less'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+import "moment/locale/zh-cn";
+moment.locale("zh-cn");
 
 Vue.config.productionTip = false
+
+Vue.use(less);
+Vue.use(Antd);
+Vue.use(VueAxios,axios);
+
+Vue.prototype.$moment = moment;
+Vue.prototype.$qs = qs;
+
+axios.defaults.baseURL = "http://localhost:3000"
+
+
 
 new Vue({
   router,
